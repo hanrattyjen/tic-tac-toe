d1b90c1ed7fb6d6abf73035faa414c546b5c22a7
hanrattyjen <hanratty.jen@gmail.com>
2016-12-01 12:01:04 +0000

    Player 2's turn is stored in an array. Tests pass.

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -30,7 +30,7 @@ describe('Game', function() {
   })
 
 
-  describe('turns', function() {
+  describe('First turn', function() {
     it('allows a player to take an initial turn', function() {
       expect(game.player1Turn(0, 0)).toEqual(1);
     });
@@ -39,24 +39,30 @@ describe('Game', function() {
       expect(game.player2Turn(0, 1)).toEqual(2);
     })
 
-    it('stores the players turn', function() {
+    it('stores player 1\s turn', function() {
       game.player1Turn(0, 0);
       expect(game.player1).toEqual([1]);
     });
+
+    it('stores player 2\'s turn', function() {
+      game.player2Turn(0, 1);
+      expect(game.player2).toEqual([2]);
+    })
+
 
     it('does not allow a player to select a spot that has an X', function() {
       game.player1Turn(0, 0);
       expect(game.player2Turn(0, 0)).toEqual("This is already taken!");
     });
+
+    it('removes that available place from the board', function() {
+      game.updateBoard(0, 0);
+      expect(game.board).toEqual([
+                        ['X', 2, 3],
+                        [4, 5, 6],
+                        [7, 8, 9]]);
+    });
+
   });
-
-
-  it('removes that available place from the board', function() {
-    game.updateBoard(0, 0);
-    expect(game.board).toEqual([
-                      ['X', 2, 3],
-                      [4, 5, 6],
-                      [7, 8, 9]]);
-  })
 
 });
