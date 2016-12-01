b49515432dba77394c81d186579ac7ce70af52e4
hanrattyjen <hanratty.jen@gmail.com>
2016-12-01 13:04:37 +0000

    Work out logic for a win. Not passing at the minute. Trying to match a player's array of choices with winning array of choices.

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -9,16 +9,16 @@ describe('Game', function() {
 
   it('shows all possible moves', function() {
     expect(game.board).toEqual([
-                      [1, 2, 3],
-                      [4, 5, 6],
-                      [7, 8, 9]]);
+                    [0,0,0],
+                    [0,0,0],
+                    [0,0,0]]);
   });
 
   it('shows current moves available', function() {
     expect(game.getCurrentBoard()).toEqual([
-                      [1, 2, 3],
-                      [4, 5, 6],
-                      [7, 8, 9]]);
+                    [0,0,0],
+                    [0,0,0],
+                    [0,0,0]]);
     });
 
   it('has player 1 starting game with no moves taken', function() {
@@ -32,37 +32,59 @@ describe('Game', function() {
 
   describe('First turn', function() {
     it('allows a player to take an initial turn', function() {
-      expect(game.player1Turn(0, 0)).toEqual(1);
+      expect(game.player1Turn(0, 0)).toEqual(0);
     });
 
     it('allows player 2 to take a turn', function() {
-      expect(game.player2Turn(0, 1)).toEqual(2);
+      expect(game.player2Turn(0, 1)).toEqual(0);
     })
 
     it('stores player 1\s turn', function() {
       game.player1Turn(0, 0);
-      expect(game.player1).toEqual([1]);
+      expect(game.player1).toEqual([[0, 0]]);
     });
 
     it('stores player 2\'s turn', function() {
       game.player2Turn(0, 1);
-      expect(game.player2).toEqual([2]);
+      expect(game.player2).toEqual([[0, 1]]);
     })
-
 
     it('does not allow a player to select a spot that has an X', function() {
       game.player1Turn(0, 0);
       expect(game.player2Turn(0, 0)).toEqual("This is already taken!");
     });
 
-    it('removes that available place from the board', function() {
-      game.updateBoard(0, 0);
+    it('removes player 1\s spot from from the board', function() {
+      game.updateBoard(0, 0, 'X');
       expect(game.board).toEqual([
-                        ['X', 2, 3],
-                        [4, 5, 6],
-                        [7, 8, 9]]);
+                        ['X', 0, 0],
+                        [0, 0, 0],
+                        [0, 0, 0]]);
     });
 
+    it('removes player 1 and player\'s taken turns from the board', function() {
+      game.updateBoard(0, 0, 'X');
+      game.updateBoard(0, 1, 'O');
+      expect(game.board).toEqual([
+                        ['X', 'O', 0],
+                        [0, 0, 0],
+                        [0, 0, 0]]);
+    })
+  });
+
+  describe('First 3 rounds of turns will give player 1 a win', function() {
+
+    beforeEach(function() {
+      game.player1Turn(0, 0);
+      game.player2Turn(1, 1);
+      game.player1Turn(1, 0);
+      game.player2Turn(0, 1);
+      game.player1Turn(2, 0);
+    });
+
+    it('allows player 1 to win', function() {
+      expect(game.player1Win()).toEqual('Win!');
+    });
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -2,11 +2,21 @@
 
 function Game() {
   this.board = [
-                  [1, 2, 3],
-                  [4, 5, 6],
-                  [7, 8, 9]];
+                  [0,0,0],
+                  [0,0,0],
+                  [0,0,0]];
   this.player1 = [];
   this.player2 = [];
+  this.win =   [
+    [[0,0], [0,1], [0,2]],
+    [[1,0], [1,1], [1,2]],
+    [[2,0], [2,1], [2,2]],
+    [[0,0], [1,0], [2,0]],
+    [[0,1], [1,1], [2,1]],
+    [[0,2], [1,2], [2,2]],
+    [[0,0], [1,1], [2,2]],
+    [[0,2], [1,1], [2,0]]
+  ];
 }
 
 Game.prototype.getCurrentBoard = function() {
@@ -15,27 +25,76 @@ Game.prototype.getCurrentBoard = function() {
 
 Game.prototype.player1Turn = function(row, column) {
   var value = this.board[row][column];
-  if (value === "X") {
-    alert("This is already taken!");
+  if (this.player1.length === 3) {
+    this.player1.unshift();
+    return player1;
+  }
+  if (value === 'X') {
+    return ("This is already taken!");
   } else {
-    this.player1.push(value);
-    this.updateBoard(row, column);
+    this.player1.push([row, column]);
+    this.updateBoard(row, column, 'X');
     return value;
   }
 }
 
 Game.prototype.player2Turn = function(row, column) {
   var value = this.board[row][column];
-  if (value === "X") {
+  if (this.player1.length === 3) {
+    this.player1.unshift();
+    return player1;
+  }
+  if (value === 'X') {
     return ("This is already taken!");
   } else {
-    this.player2.push(value);
-    this.updateBoard(row, column);
+    this.player2.push([row, column]);
+    this.updateBoard(row, column, 'O');
     return value;
   }
 }
 
-Game.prototype.updateBoard = function(row, column) {
-  this.board[row][column] = 'X';
+Game.prototype.updateBoard = function(row, column, turn) {
+  this.board[row][column] = turn;
   return this.board;
 }
+
+Game.prototype.player1Win = function() {
+  var arrayLength = this.win.length;
+  for (var i = 0; i < arrayLength; i++) {
+    console.log(this.player1.diff(this.win[i]));
+    console.log(this.win[i].diff(this.player1));
+
+    // console.log(this.win[i] - this.player1);
+    // console.log(this.win[i]);
+      // if (this.win[i] === this.player1) {
+      //   console.log('yes');
+      // }
+  }
+}
+
+
+
+Game.prototype.player2Win = function() {
+
+}
+
+//
+// win object
+// player 1 win
+// board = ['X', 'X', 'X'], [4, 5, 6], [7, 8, 9]
+// board = [1, 2, 3], ['X', 'X', 'X'], [7, 8, 9]
+// board = [1, 2, 3], [4, 5, 6], ['X', 'X', 'X']
+// board = ['X', 2, 3], ['X', 5, 6], ['X', 8, 9]
+// board = [1, 'X', 3], [4, 'X', 6], [7, 'X', 9]
+// board = [1, 2, 'X'], [3, 4, 'X'], [7, 8, 'X']
+// board = ['X', 2, 3], [4, 'X', 6], [7, 8, 'X']
+// board = [1, 2, 'X'], [4, 'X', 6], ['X', 8, 9]
+// player 2 win
+// board = ['O', 'O', 'O'], [4, 5, 6], [7, 8, 9]
+// board = [1, 2, 3], ['O', 'O', 'O'], [7, 8, 9]
+// board = [1, 2, 3], [4, 5, 6], ['O', 'O', 'O']
+// board = ['O', 2, 3], ['O', 5, 6], ['O', 8, 9]
+// board = [1, 'O', 3], [4, 'O', 6], [7, 'O', 9]
+// board = [1, 2, 'O'], [3, 4, 'O'], [7, 8, 'O']
+// board = ['O', 2, 3], [4, 'O', 6], [7, 8, 'O']
+// board = [1, 2, 'O'], [4, 'O', 6], ['O', 8, 9]
